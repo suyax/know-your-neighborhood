03d4d0bc005a44b3f2eeaa026779835fcf91ccac
suya xu <tataxusuya@hotmail.com>
2016-02-18 15:19:30 -0800

    modify server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
-var path = require('path')
+var port = process.env.PORT || 3000;
 
-// connect to mongo database named "shortly"
-mongoose.connect('mongodb://localhost/Myapp');
+//mongoose.connect('mongodb://localhost/Myapp');
 
 // configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
 //require('./config/routes.js')(app, express);
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/../client/index.html'));
-// });
+app.get('/', function(req, res) {
+     res.sendFile(path.join(__dirname + '/../client/index.html'));
+});
 // start listening to requests on port 8000
 
 
